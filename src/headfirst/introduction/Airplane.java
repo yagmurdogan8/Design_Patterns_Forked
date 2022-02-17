c7de2f44b215c3b148232d285bedb2765ad61023
Ali Yürekli <yurekli.ali@gmail.com>
2022-02-17 13:16:27 +0300

    Sample project created.

---
+++ src/headfirst/introduction/Airplane.java
@@ -0,0 +1,17 @@
+package headfirst.introduction;
+
+public class Airplane {
+
+    private int speed;
+
+    public Airplane() {
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
